0d45ed87d835db4c315934f295a1f578af3e9e06
Vladyslav Yavorskiy <vladyavorskii295@gmail.com>
2025-12-14 19:35:00 +0200

    Initialize Angular standalone project with working navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('YavorskyiAngularProject');
-}
+export class AppComponent {}
