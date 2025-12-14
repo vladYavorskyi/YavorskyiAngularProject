d58d1b67e177c0d6b8b1e0993e0433977a239909
Vladyslav Yavorskiy <vladyavorskii295@gmail.com>
2025-12-14 20:55:55 +0200

    Add habit creation form with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HabitListComponent } from './components/habit-list/habit-list';
+import { HabitFormComponent } from './components/habit-form/habit-form';
 
 export const routes: Routes = [
-  { path: '', component: HabitListComponent }
+  { path: '', component: HabitListComponent },
+  { path: 'add', component: HabitFormComponent }
 ];
---
+++ src/app/components/habit-form/habit-form.ts
@@ -0,0 +1,29 @@
+import { Component, signal, inject } from '@angular/core';
+import { HabitService } from '../../services/habit';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-habit-form',
+  standalone: true,
+  templateUrl: './habit-form.html'
+})
+export class HabitFormComponent {
+  private habitService = inject(HabitService);
+  private router = inject(Router);
+
+  title = signal('');
+  description = signal('');
+
+  save() {
+    if (!this.title()) return;
+
+    this.habitService.add({
+      id: Date.now(),
+      title: this.title(),
+      description: this.description(),
+      createdAt: new Date().toISOString()
+    });
+
+    this.router.navigateByUrl('/');
+  }
+}
